17e8156004af540c0c441564226f423c301203ae
COL073 <nicolecolleta9@gmail.com>
2024-02-24 12:18:56 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# user_input.py
+
+# Ask user for their name and store it in a variable called "name"
+name = input("What is your name? ")
+# Ask user for their age and store it in a variable called "age"
+age = input("How old are you? ")
+# Ask user where they live and store it in a variable called "location"
+location = input("Where do you live?")
+print(f"My name is {'Peter Tyler'}, I am {20} years old and I live in {'Shaurimoyo'}.")
+
